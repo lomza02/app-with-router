f77060fcf58b2b4760f248a04c2a3ac0431b2ee8
Mic Lom <mlomzynski94@gmail.com>
2020-05-24 17:24:24 +0200

    Updates

---
+++ precache-manifest.01d67b31ab457f86ac388108cf828c82.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f900e4e2188bcfd3ae624f1caea08476",
+    "url": "/app-with-router/index.html"
+  },
+  {
+    "revision": "98cbf2891054ffb6929a",
+    "url": "/app-with-router/static/css/main.3e186346.chunk.css"
+  },
+  {
+    "revision": "a52ce73324cb6b4ea202",
+    "url": "/app-with-router/static/js/2.4086674d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-with-router/static/js/2.4086674d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98cbf2891054ffb6929a",
+    "url": "/app-with-router/static/js/main.7335ded5.chunk.js"
+  },
+  {
+    "revision": "02c2e52b8b4af988a33b",
+    "url": "/app-with-router/static/js/runtime-main.dc212790.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-with-router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-with-router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-with-router/static/media/header3.d9d36b65.jpg"
+  }
+]);
